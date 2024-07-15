860cec649290a7a46f6bccf2a3f384fec48d0004
Gabriel Batista <gabriel.rodrigues250306@gmail.com>
2024-07-15 13:35:16 -0300

    Atualizado

--- Curso em video/Exercícios de Python/ex0034.py
+++
@@ -1,6 +0,0 @@
-salario = float(input('Qual o seu salário? R$: '))
-if salario <= 1250:
-    novo = salario + (salario * 15 / 100)
-else:
-    novo = salario + (salario * 10 / 100)
-print('Você ganhava {:.2f}\n Agora você irá começar a receber {:.2f} R$ agora'.format(salario, novo))
